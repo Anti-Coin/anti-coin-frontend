f9bd5edb8d08c50dd6a244eeb57fc24b0c13bfdb
dongwonmoon <ansehddnjs187@gmail.com>
2026-01-27 15:37:40 +0900

    SSG locust

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -5,24 +5,29 @@ import json
 class CoinUser(HttpUser):
     wait_time = between(1, 3)
 
+    # @task
+    # def predict_btc(self):
+    #     with self.client.get("/predict/BTC/USDT", catch_response=True) as response:
+
+    #         if response.status_code == 0:
+    #             print(f"\nStatus 0 Detected!")
+    #             print(f"Reason: {response.error}")
+    #             response.failure(f"Network Fail: {response.error}")
+    #             return
+
+    #         if response.status_code != 200:
+    #             print(
+    #                 f"[ERROR] Status: {response.status_code} | Body: {response.text[:50]}..."
+    #             )
+    #             response.failure(f"Status {response.status_code}")
+    #             return
+
+    #         try:
+    #             response.json()
+    #         except json.JSONDecodeError:
+    #             response.failure("JSON Decode Error")
+                
     @task
-    def predict_btc(self):
-        with self.client.get("/predict/BTC/USDT", catch_response=True) as response:
-
-            if response.status_code == 0:
-                print(f"\nStatus 0 Detected!")
-                print(f"Reason: {response.error}")
-                response.failure(f"Network Fail: {response.error}")
-                return
-
-            if response.status_code != 200:
-                print(
-                    f"[ERROR] Status: {response.status_code} | Body: {response.text[:50]}..."
-                )
-                response.failure(f"Status {response.status_code}")
-                return
-
-            try:
-                response.json()
-            except json.JSONDecodeError:
-                response.failure("JSON Decode Error")
+    def get_static_prediction(self):
+        with self.client.get("/static/prediction_BTC_USDT.json", catch_response=True) as response:
+            pass
